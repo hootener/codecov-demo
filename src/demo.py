d922286cd6b46337af5eb8f4da6f612f3f2603e2
Eli Hooten <elir.hooten@gmail.com>
2019-11-13 19:02:32 -0800

    Dramatically Reduce Coverage

--- src/demo.py
+++ src/demo.py
@@ -3,3 +3,15 @@ def untested_function():
 
 def tested_function():
     return True
+
+
+def new_func_1():
+    return False
+
+
+def new_func_2():
+    return False
+
+
+def new_func_3():
+    return False
--- src/test_index.py
+++ src/test_index.py
@@ -4,9 +4,3 @@ import index
 def test_uncovered_if():
     assert index.uncovered_if() == False
 
-def test_fully_covered():
-    assert index.fully_covered() == True
-
-
-
-
